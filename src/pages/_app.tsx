241e98c843edbb98601ab663572d2612431837a0
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2023-04-21 14:58:39 +0200

    Add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { MantineProvider } from "@mantine/core";
+import Script from 'next/script';
 
 import { api } from "~/utils/api";
 
@@ -13,15 +14,29 @@ const MyApp: AppType<{ session: Session | null }> = ({
                                                        pageProps: { session, ...pageProps }
                                                      }) => {
   return (
-    <SessionProvider session={session}>
-      <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: "dark" }}>
-        <div className={"flex flex-col p-8 max-w-[72.5rem] mx-auto"}>
-          <Menu />
+    <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || ""}`} />
 
-          <Component {...pageProps} />
-        </div>
-      </MantineProvider>
-    </SessionProvider>
+      <Script strategy="lazyOnload" id="google-analytics">
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || ""}');
+            `}
+      </Script>
+
+      <SessionProvider session={session}>
+        <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: "dark" }}>
+          <div className={"flex flex-col p-8 max-w-[72.5rem] mx-auto"}>
+            <Menu />
+
+            <Component {...pageProps} />
+          </div>
+        </MantineProvider>
+      </SessionProvider>
+    </>
   );
 };
 
