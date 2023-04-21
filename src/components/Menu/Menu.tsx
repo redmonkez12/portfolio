99d89fcb7346ef44be87853e320377a6745b24cb
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2023-04-21 15:41:09 +0200

    Add coffee

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -43,7 +43,13 @@ export function Menu() {
         onClick={menuOpen} />
 
       <Group className={"gap-4 justify-between"}>
-        <Image src={"/images/logo.svg"} width={45} height={45} alt={"logo"} />
+        <div className={"flex gap-4 items-center"}>
+          <Image src={"/images/logo.svg"} width={50} height={50} alt={"logo"} />
+          <a href="https://www.buymeacoffee.com/redmonkez12" target="_blank">
+            <Image src={"/images/buy-coffee.png"} width={160} height={45} alt="Buy Me A Coffee"
+            />
+          </a>
+        </div>
 
         <Group className={`flex-col w-full md:w-auto
         md:flex-row ${showMenu ? "" : "hidden"} absolute md:relative z-10 md:flex bg-[#1A1B1E] top-0 bottom-0 left-0 right-0 p-8 md:p-0`}>
